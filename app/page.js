f5f8d4073c196917ec20901d50c51ddc130cf808
Swastik Yadav <swastikb380@gmail.com>
2024-03-23 11:18:23 +0530

    add posts and footer section on home page

--- app/page.js
+++ app/page.js
@@ -14,18 +14,42 @@ export default function Home() {
       <h1 className="pt-6 pb-2 text-4xl font-bold w-full">Keyboard</h1>
       <h2 className="pb-6 text-normal font-bold w-full text-purple-500 border-b border-border">/ˈkiːbɔːrd/</h2>
 
-      {/* <p>Start your blog in <span className="text-purple-800 font-bold dark:text-purple-500">few hours not few days.</span></p>
-      <small className="my-4">
-        Ready to go, <span className="text-purple-800 font-bold dark:text-purple-500">done for you NextJs blog setup</span>.
-        Clone, write your first blog, and deploy. As simple as that.
-        <br /><br />
-        Focus on writing, not on setting up the blog.
-        <br /><br />
-        New templates launching soon.
-      </small>
-      <Link href="/blog">
-        <Button>Read blogs</Button>
-      </Link> */}
+      <section className="w-full py-6 border-b border-border">
+        <p className="font-bold">Description:</p>
+        <br />
+        <p>
+          A set of keys used to operate a typewriter, computer etc.
+          Components of many instruments including the piano, organ, and harpsichord
+          consisting of usually black and white keys that cause different tones to be
+          produced when struck.
+          A device with keys of a musical keyboard, used to control electronic sound-producing
+          devices which may be built into or separate from the keyboard device.
+        </p>
+      </section>
+
+      <section className="w-full py-6 border-b border-border">
+        {
+          [
+            {title: "Term one", description: "Small description about the term."},
+            {title: "React composition pattern", description: "Composition in react is a pattern of building reusable components."},
+            {title: "Inheritance", description: "Inheritance is a programming concept of sharing functionality between components."},
+            {title: "Test headline", description: "Testing the list of programming terms, concepts, and patterns."},
+          ].map((post) => {
+            return (
+              <div className="py-2">
+                <h3 className="text-lg font-bold text-purple-500">{post.title} &rarr;</h3>
+                <p className="text-slate-500">{post.description}</p>
+              </div>
+            );
+          })
+        }
+      </section>
+
+      <footer className="w-full py-6">
+        <small>Adding new concepts regularly. Join the <a className="text-purple-500" href="https://diaryofadev.ck.page" target="_blank">Newsletter</a> to get updates.</small>
+        <br />
+        <small>Built for developers by <a className="text-purple-500" href="https://swastikyadav.com" target="_blank">Swastik Yadav</a>.</small>
+      </footer>
     </main>
   );
 }
